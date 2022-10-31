fc2f6d64a4f1eb386acfdcc59fd63a9c09291950
Lukas Heilmann <lukas@familieheilmann.de>
2022-10-31 17:51:38 +0100

    Update client.js

--- supabase/client.js
+++ supabase/client.js
@@ -3,5 +3,14 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, { headers: { cache: 'no-store' } })
+async function customFetch(url, options) {
+  console.log(url, options);
+  const customOptions = {
+    cache: "no-cache",
+    ...options
+  }
+  return fetch(url, customOptions);
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, { fetch: customFetch})
 
