a4b875102c36eadb1b1e56fb9e9dd67d225e930c
Lukas Heilmann <lukas@familieheilmann.de>
2022-11-17 18:09:00 +0100

    Add coming coon

--- app/RankingCard.tsx
+++ app/RankingCard.tsx
@@ -1,27 +1,36 @@
 "use client";
 
-import { Field, Form, Formik } from "formik";
-import {
-  useUser,
-  useSupabaseClient,
-  useSessionContext,
-} from "@supabase/auth-helpers-react";
-import supabase from "../supabase";
-import { useRouter } from "next/navigation";
-import Script from "next/script";
-
 export default function RankingCard() {
   return (
     <>
-      <div className="flex flex-col p-4 rounded-lg border border-zinc-700 bg-zinc-800">
-        <span className="flex text-zinc-500 uppercase tracking-wider">
-          Ranking
-        </span>
-        <div className="flex flex-col items-start justify-center py-4 gap-4">
-          <span>Name 1</span>
-          <span>Name 2</span>
-          <span>Name 3</span>
+      <div className="relative flex flex-col p-4 rounded-lg border border-zinc-700 bg-zinc-800">
+        <div className="flex gap-4 items-center">
+          <span className="flex text-zinc-500 uppercase tracking-wider">
+            Ranking
+          </span>
+          <div className="flex items-center justify-center bg-zinc-700 px-1.5 py-0.5 rounded">
+            <span className="text-sm text-zinc-400">coming soon</span>
+          </div>
         </div>
+        <div className="flex flex-col py-4 gap-2">
+          <div className="flex flex-row w-full items-center justify-between">
+            <span>skdjhfsd</span>
+            <span className="font-mono tracking-wider">7040</span>
+          </div>
+          <div className="flex flex-row w-full items-center justify-between">
+            <span>sdkfsdfsdf</span>
+            <span className="font-mono tracking-wider">1508</span>
+          </div>
+          <div className="flex flex-row w-full items-center justify-between">
+            <span>skd_jh1</span>
+            <span className="font-mono tracking-wider">784</span>
+          </div>
+          <div className="flex flex-row w-full items-center justify-between">
+            <span>TTEfvhdv88</span>
+            <span className="font-mono tracking-wider">500</span>
+          </div>
+        </div>
+        <div className="absolute inset-x-2 top-11 bottom-2 backdrop-blur-sm z-90"></div>
       </div>
     </>
   );
