2a138f3cfc4ca6de6f89c1b35cd944e851bc3a5c
Lukas Heilmann <lukas@familieheilmann.de>
2022-11-17 23:06:52 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,8 @@ export default async function Page() {
 
   return (
     <div className="flex flex-col gap-10 w-full">
-      <section className="flex items-end justify-center pattern bg-transparent min-h-[200px] sm:min-h-[300px]">
-        <h1 className="text-6xl pb-4 sm:pb-20 sm:text-8xl text-center tracking-wider text-emerald-100 uppercase text-transparent bg-clip-text bg-gradient-to-r from-emerald-500/75 via-emerald-200/90 to-emerald-500/75">
+      <section className="flex items-end justify-center pattern bg-transparent min-h-[300px] sm:min-h-[340px]">
+        <h1 className="text-6xl pb-8 sm:pb-20 sm:text-8xl text-center tracking-wider text-emerald-100 uppercase text-transparent bg-clip-text bg-gradient-to-r from-emerald-500/75 via-emerald-200/90 to-emerald-500/75">
           Mate in Berlin
         </h1>
       </section>
