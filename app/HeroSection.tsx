f9b492f6fb7467526e62fa91cecf13ef4c701547
Lukas Heilmann <lukas@familieheilmann.de>
2022-11-19 00:16:10 +0100

    Update

--- app/HeroSection.tsx
+++ app/HeroSection.tsx
@@ -2,7 +2,7 @@ export default function HeroSection() {
   return (
     <section className="flex items-center justify-center pattern bg-transparent min-h-[300px] sm:min-h-[340px]">
       <h1 className="text-6xl sm:text-8xl text-center tracking-wider text-lila-100 text-transparent bg-clip-text bg-gradient-to-r from-lila-500/75 via-lila-200/90 to-lila-500/75">
-        mate in Berlin
+        MATE IN BERLIN
       </h1>
     </section>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,20 @@
 import "./global.css";
 import React from "react";
 
-import { Inter } from "@next/font/google";
+import { Montserrat } from "@next/font/google";
 import clsx from "clsx";
 import Provider from "./Provider";
 import Header from "./Header";
-import InstagramLogoIcon from "/instagram-icon.svg";
 import Footer from "./Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 type Props = {
   children: React.ReactNode;
 };
 export default async function RootLayout(props: Props) {
   return (
-    <html lang="de" className={clsx("p-0 m-0", inter.className)}>
+    <html lang="de" className={clsx("p-0 m-0", montserrat.className)}>
       <head>
         <title>Mate In Berlin</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
