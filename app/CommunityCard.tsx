18a1310bf3f0ec76b0eeb2ab6b4b0c9c1f3fd805
Lukas Heilmann <lukas@familieheilmann.de>
2022-11-20 15:28:23 +0100

    Update CommunityCard.tsx

--- app/CommunityCard.tsx
+++ app/CommunityCard.tsx
@@ -11,18 +11,22 @@ export default function CommunityCard(props: Props) {
         <span className="flex text-lila-400 uppercase tracking-wider">
           Community
         </span>
-        <div className="flex flex-col items-start justify-center py-4 gap-4">
-          <div className="flex flex-1 items-end justify-start gap-2">
-            <span className="text-6xl text-lila-100">
-              {props.numberOfTournaments}
-            </span>
-            <span className="text-lila-400">Turniere</span>
+        <div className="flex flex-1 flex-col items-start justify-center py-4 gap-4">
+          <div className="flex flex-1 items-center">
+            <div className="flex items-end justify-end gap-3">
+              <span className="text-6xl text-lila-100">
+                {props.numberOfTournaments}
+              </span>
+              <span className="text-lg text-lila-300">Turniere</span>
+            </div>
           </div>
           <div className="flex flex-1 items-end justify-start gap-2">
-            <span className="text-6xl text-lila-100">
-              {props.numberOfProfiles}
-            </span>
-            <span className="text-lila-400">Spieler*innen</span>
+            <div className="flex items-end justify-end gap-3">
+              <span className="text-6xl text-lila-100">
+                {props.numberOfProfiles}
+              </span>
+              <span className="text-lila-300 text-lg">Spieler*innen</span>
+            </div>
           </div>
         </div>
       </div>
