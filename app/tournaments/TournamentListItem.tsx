77c67b2ebebfbed57fe38ffb5c9834a80f95ff73
Lukas Heilmann <lukas@familieheilmann.de>
2022-11-21 22:57:35 +0100

    Fix invalid range

--- app/tournaments/TournamentListItem.tsx
+++ app/tournaments/TournamentListItem.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { format } from "date-fns";
+import { format, setHours } from "date-fns";
 import { de } from "date-fns/locale";
 import { ArrowSmallRightIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
@@ -15,27 +15,28 @@ export default function TournamentListItem(props: Props) {
     router.push(`/tournaments/${tournamentId}`);
   };
 
+  const [hours] = props.nextTournament.time.split(":");
+  const dateTime = format(
+    setHours(new Date(props.nextTournament.date), hours),
+    "PPPp",
+    {
+      locale: de,
+    }
+  );
+
   return (
     <div
-      className="group flex justify-between rounded-lg border border-primary-700 bg-primary-800 p-4 pr-6 w-full cursor-pointer hover:bg-primary-700 transition"
+      className="group flex w-full cursor-pointer justify-between rounded-lg border border-primary-700 bg-primary-800 p-4 pr-6 transition hover:bg-primary-700"
       onClick={() => navigateToTournament(props.tournament.id)}
     >
       <div className="flex flex-col">
-        <h2 className="text-xl text-primary-100">
-          {format(
-            new Date(props.tournament.date + " " + props.tournament.time),
-            "PPPp",
-            {
-              locale: de,
-            }
-          )}
-        </h2>
+        <h2 className="text-xl text-primary-100">{dateTime}</h2>
         <p className="text-primary-300">
           <span>{props.tournament.location}</span>
         </p>
       </div>
       <div className="flex items-center">
-        <ArrowSmallRightIcon className="h-6 w-6 text-primary-100 group-hover:translate-x-2 transition" />
+        <ArrowSmallRightIcon className="h-6 w-6 text-primary-100 transition group-hover:translate-x-2" />
       </div>
     </div>
   );
