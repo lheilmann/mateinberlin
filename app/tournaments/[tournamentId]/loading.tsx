a2bcf31251f21d18ef08e7a93cf4065b2459e430
Lukas Heilmann <lukas@familieheilmann.de>
2022-12-01 19:39:32 +0100

    Update loading.tsx

--- app/tournaments/[tournamentId]/loading.tsx
+++ app/tournaments/[tournamentId]/loading.tsx
@@ -2,10 +2,14 @@ import Skeleton from "~components/Skeleton";
 
 export default function Loading() {
   return (
-    <div className="flex w-full flex-col gap-4">
-      <Skeleton className="h-8 w-60 rounded-lg"></Skeleton>
-      <Skeleton className="h-8 w-60 rounded-lg"></Skeleton>
-      <Skeleton className="h-8 w-60 rounded-lg"></Skeleton>
+    <div className="flex w-full flex-col gap-8">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-4">
+        <div className="flex flex-col gap-4">
+          <Skeleton className="h-8 w-full rounded-lg"></Skeleton>
+          <Skeleton className="h-8 w-full rounded-lg"></Skeleton>
+          <Skeleton className="h-8 w-full rounded-lg"></Skeleton>
+        </div>
+      </div>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-4">
         <Skeleton className="h-60 w-full rounded-lg"></Skeleton>
         <Skeleton className="h-60 w-full rounded-lg sm:col-span-3"></Skeleton>
