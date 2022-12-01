2622e645e924af171c4fc1a79ed99b4baf4a249d
Lukas Heilmann <lukas@familieheilmann.de>
2022-12-01 19:30:48 +0100

    Remve plus

--- app/tournaments/[tournamentId]/page.tsx
+++ app/tournaments/[tournamentId]/page.tsx
@@ -74,20 +74,6 @@ export default async function Page(props: Props) {
                 {participant.profiles.username}
                 {participant.has_board && (
                   <Badge>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="h-4 w-4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 4.5v15m7.5-7.5h-15"
-                      />
-                    </svg>
                     <span>Schachbrett</span>
                   </Badge>
                 )}
